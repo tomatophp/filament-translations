a78aa173fc19a5a4d64eb6ad416777d8198b6f32
Baspa <hello@baspa.dev>
2024-05-17 10:36:24 +0200

    wip

--- src/Jobs/ScanJob.php
+++ src/Jobs/ScanJob.php
@@ -20,12 +20,11 @@ class ScanJob implements ShouldQueue
     public function handle(): void
     {
         if (config('filament-translations.path_to_custom_import_command')) {
-            $command
-                = new config('filament-translations.path_to_custom_import_command');
+            $command = config('filament-translations.path_to_custom_import_command');
             $command->handle();
             return;
         }
-        
+
         $saveScan = new SaveScan();
         $saveScan->save();
     }
