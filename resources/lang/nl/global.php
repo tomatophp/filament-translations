2668b6855e8fa283ff38b28f09f0f6c781aa0c9c
Baspa <hello@baspa.dev>
2024-05-17 10:18:35 +0200

    Add Dutch translations

---
+++ resources/lang/nl/global.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "created_at" => "Aangemaakt op",
+    "updated_at" => "Bijgewerkt op",
+    "filter_by_group" => "Filteren op Groep",
+    "filter_by_null_text" => "Filteren op Lege Tekst",
+];
---
+++ resources/lang/nl/translation.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    "title" => [
+        "home" => "Translations",
+        "list" => "Translations",
+        "create" => "Create Translation",
+        "edit" => "Edit Translation",
+    ],
+    "label" => "Translations",
+    "group" => "Translations",
+    "key" => "Key",
+    "text" => "Text",
+    "scan" => "Scan For New Languages",
+    "namespace" => "Namespace",
+    "notification" => "Language has been changed succcess",
+    "loaded" => "Translations list has been updated",
+    "uploaded" => "Translations has been uploaded successfully",
+    "import" => "Import",
+    "export" => "Export",
+    "import-file" => "Select Exported File After Translate It",
+    "menu" => "Change Language",
+    "modal" => [
+        "heading" => "Update current language",
+        "button" => "Save, language",
+        "setting" => "Settings",
+        "select" => "Language",
+    ],
+    "lang" => [
+        "ar" => "Arabic",
+        "en" => "English",
+        "pt_BR" => "Português (Brasil)",
+        "my" => "Burmese",
+        "nl" => "Dutch",
+    ]
+];
