dd37ee636f2d250d8e6c1886728890f5f055d003
Fady Mondy <info@3x1.io>
2024-09-17 19:44:21 +0300

    fix ->isEnabled()

--- src/FilamentTranslationsPlugin.php
+++ src/FilamentTranslationsPlugin.php
@@ -26,10 +26,8 @@ class FilamentTranslationsPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        if(class_exists(Module::class)){
-            if(\Nwidart\Modules\Facades\Module::find('FilamentTranslations')?->isEnabled()){
-                $this->isActive = true;
-            }
+        if(class_exists(Module::class) && \Nwidart\Modules\Facades\Module::find('FilamentTranslations')?->isEnabled()){
+            $this->isActive = true;
         }
         else {
             $this->isActive = true;
